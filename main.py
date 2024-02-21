17e597b4b000a1a7d467dd19acddc958670d6c03
Normanton <normanton100@gmail.com>
2024-02-21 16:27:53 +0600

    pars all name

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import csv
 from model import Product
 headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
-def parser(url:str):
+def parser(url:str, max_item: int):
     create_csv()
-    list_product = []
-    res = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(res.text, "lxml")
-    products = soup.find_all("div", class_="info pt-3")
-    for product in products:
-        name = product.find("a").text
-        print(name)
-        list_product.append(Product(name=name))
-    write_csv(list_product)
+    page = 1
+    count_items = 0
+    while max_item > count_items:
+        list_product = []
+        res = requests.get(f"{url}?q=yacht-charter.html&page={page}", headers=headers)
+        soup = BeautifulSoup(res.text,"lxml")
+        products = soup.find_all("div", class_="info pt-3")
+
+        for product in products:
+            if count_items >= max_item:
+                break
+            count_items += 1
+            name = product.find("a").text
+            print(name)
+            list_product.append(Product(name=name))
+        write_csv(list_product)
+        page += 1
 
 
 def create_csv():
@@ -33,5 +41,5 @@ def write_csv(products: list[Product]):
             ])
 
 if __name__ == "__main__":
-    parser(url="https://luxury-yacht-brokerage.com/yacht/yacht-charter.html?q=yacht%2Fyacht-charter.html&page=1")
+    parser(url="https://luxury-yacht-brokerage.com/yacht-charter.html", max_item=40)
 
