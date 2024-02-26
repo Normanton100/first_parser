1762786f725377d045f1e0e8ca3a0947be439afc
Normanton <normanton100@gmail.com>
2024-02-26 11:20:22 +0600

    Writing to csv works

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from model import Product
-headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+from fake_useragent import UserAgent
+headers = UserAgent()
 
 def parser(url:str, max_item: int):
     create_csv()
@@ -10,7 +11,7 @@ def parser(url:str, max_item: int):
     count_items = 0
     while max_item > count_items:
         list_product = []
-        res = requests.get(f"{url}?q=yacht-charter.html&page={page}", headers=headers)
+        res = requests.get(f"{url}?q=yacht-charter.html&page={page}", headers={'user-agent':f'{headers.random}'})
         soup = BeautifulSoup(res.text,"lxml")
         products = soup.find_all("div", class_="info pt-3")
 
@@ -18,26 +19,125 @@ def parser(url:str, max_item: int):
             if count_items >= max_item:
                 break
             count_items += 1
-            name = product.find("a").text
+            name = product.find("a").get("href")
+
             print(name)
-            list_product.append(Product(name=name))
+            res_page = requests.get(f"https://luxury-yacht-brokerage.com{name}", headers={'user-agent':f'{headers.random}'})
+            #print(res_page)
+            soup_page = BeautifulSoup(res_page.text, "lxml")
+            title = soup_page.find(class_="is-visible").text[3:]
+            price = soup_page.find("span", class_="counter").text
+            # description = soup_page.
+            # tags = soup_page.
+            # #gallery = soup_page.
+            # advantageName_1 = soup_page.
+            # advantageValue_1 = soup_page.
+            # advantageName_2 = soup_page.
+            # advantageValue_2 = soup_page.
+            # advantageName_3 = soup_page.
+            # advantageValue_3 = soup_page.
+            # advantageName_4 = soup_page.
+            # advantageValue_4 = soup_page.
+            # advantageName_5 = soup_page.
+            # advantageValue_5 = soup_page.
+            # advantageName_6 = soup_page.
+            # advantageValue_6 = soup_page.
+            # advantageName_7 = soup_page.
+            # advantageValue_7 = soup_page.
+            # advantageName_8 = soup_page.
+            # advantageValue_8 = soup_page.
+            # advantageName_9 = soup_page.
+            # advantageValue_9 = soup_page.
+
+            list_product.append(Product(title=title,
+                                        price=price,
+                                        # description=description,
+                                          # tags=tags,
+                                          # #gallery=gallery,
+                                          # advantageName_1=advantageName_1,
+                                          # advantageValue_1=advantageValue_1,
+                                          # advantageName_2=advantageName_2,
+                                          # advantageValue_2=advantageValue_2,
+                                          # advantageName_3=advantageName_3,
+                                          # advantageValue_3=advantageValue_3,
+                                          # advantageName_4=advantageName_4,
+                                          # advantageValue_4=advantageValue_4,
+                                          # advantageName_5=advantageName_5,
+                                          # advantageValue_5=advantageValue_5,
+                                          # advantageName_6=advantageName_6,
+                                          # advantageValue_6=advantageValue_6,
+                                          # advantageName_7=advantageName_7,
+                                          # advantageValue_7=advantageValue_7,
+                                          # advantageName_8=advantageName_8,
+                                          # advantageValue_8=advantageValue_8,
+                                          # advantageName_9=advantageName_9,
+                                          # advantageValue_9=advantageValue_9
+                                        ))
+            print(title)
+            print(price)
         write_csv(list_product)
+
         page += 1
+
 
 
 def create_csv():
     with open(f"glavsnab.csv", mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([
-            "name",
-        ])
+            "title",
+            "price"
+            # "description",
+            # "tags",
+            # #"gallery",
+            # "advantageName_1",
+            # "advantageValue_1",
+            # "advantageName_2",
+            # "advantageValue_2",
+            # "advantageName_3",
+            # "advantageValue_3",
+            # "advantageName_4",
+            # "advantageValue_4",
+            # "advantageName_5",
+            # "advantageValue_5",
+            # "advantageName_6",
+            # "advantageValue_6",
+            # "advantageName_7",
+            # "advantageValue_7",
+            # "advantageName_8",
+            # "advantageValue_8",
+            # "advantageName_9",
+            # "advantageValue_9"
+            ])
 
-def write_csv(products: list[Product]):
+def write_csv(soup_page: list[Product]):
     with open(f"glavsnab.csv", mode="a", newline="", encoding='utf-8') as file:
         writer = csv.writer(file)
-        for product in products:
+        for product in soup_page:
             writer.writerow([
-                product.name
+                product.title,
+                product.price
+                # product.description,
+                # product.tags,
+                # #product.gallery,
+                # product.advantageName_1,
+                # product.advantageValue_1,
+                # product.advantageName_2,
+                # product.advantageValue_2,
+                # product.advantageName_3,
+                # product.advantageValue_3,
+                # product.advantageName_4,
+                # product.advantageValue_4,
+                # product.advantageName_5,
+                # product.advantageValue_5,
+                # product.advantageName_6,
+                # product.advantageValue_6,
+                # product.advantageName_7,
+                # product.advantageValue_7,
+                # product.advantageName_8,
+                # product.advantageValue_8,
+                # product.advantageName_9,
+                # product.advantageValue_9
             ])
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -2,4 +2,26 @@ from dataclasses import dataclass
 
 @dataclass
 class Product:
-    name: str
+    title: str
+    price: str
+#     description: str
+#     tags: str
+# #    gallery: str
+#     advantageName_1: str
+#     advantageValue_1: str
+#     advantageName_2: str
+#     advantageValue_2: str
+#     advantageName_3: str
+#     advantageValue_3: str
+#     advantageName_4: str
+#     advantageValue_4: str
+#     advantageName_5: str
+#     advantageValue_5: str
+#     advantageName_6: str
+#     advantageValue_6: str
+#     advantageName_7: str
+#     advantageValue_7: str
+#     advantageName_8: str
+#     advantageValue_8: str
+#     advantageName_9: str
+#     advantageValue_9: str
